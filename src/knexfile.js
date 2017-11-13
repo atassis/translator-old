5f3795202305f70d168e76f3b60dd196790b9570
t.kaytmazov <t.kaytmazov@biiletix.ru>
2017-11-13 22:17:42 +0300

    Removed unnecessary properties

--- src/knexfile.js
+++ src/knexfile.js
@@ -1,15 +1,6 @@
 const { db: { connection } } = require('./configuration');
 
 module.exports = {
-  client: 'mysql',
-  connection,
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-  },
   development: {
     client: 'mysql',
     connection,
