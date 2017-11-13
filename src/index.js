50817249a657a18485ba68e21fa75e45470f59fa
t.kaytmazov <t.kaytmazov@biiletix.ru>
2017-11-13 22:17:14 +0300

    Added dependencies. Added body parser

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ process.env.NODE_PATH = __dirname;
 require('module').Module._initPaths(); // eslint-disable-line no-underscore-dangle
 const { createServer } = require('http');
 const Koa = require('koa');
+const koaBody = require('koa-body');
 const config = require('configuration');
 const controllers = require('controllers');
 
@@ -10,13 +11,13 @@ const app = new Koa();
 app.proxy = true;
 app.keys = [config.secret];
 
+app.use(koaBody());
 app.use(controllers.routes());
 app.use(controllers.allowedMethods());
 
 const server = createServer(app.callback());
 
 server.listen(config.port, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log(`App is running on ${ host }:${ port }, development is ${ config.development }`);
+  const { address, port } = server.address();
+  console.log(`App is running on ${ address }:${ port }, development is ${ config.development }`);
 });
